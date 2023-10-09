8973369378d53c7fb79483986c38667d85b8f4cc
9492277468+-Aa <idiotcommerce@gmail.com>
2023-10-09 16:00:18 +0530

    reversed the words in the string

--- test.py
+++ test.py
@@ -22,27 +22,48 @@
 
 # prime number
 
-def prime_numbers(N):
-    prime_list = [2]
+# def prime_numbers(N):
+#     prime_list = [2]
 
-    for number in range(3, N+1):
-        if not has_factors(number):
-            prime_list.append(number)
-    return prime_list
+#     for number in range(3, N+1):
+#         if not has_factors(number):
+#             prime_list.append(number)
+#     return prime_list
 
-def has_factors(number):
+# def has_factors(number):
 
-    check_upto = ( number // 2) + 1
+#     check_upto = ( number // 2) + 1
 
-    for item in range(2, check_upto):
-        if number % item == 0:
-            return True
-    return False
+#     for item in range(2, check_upto):
+#         if number % item == 0:
+#             return True
+#     return False
 
-print(prime_numbers(100))
+# print(prime_numbers(100))
 
 
+# -----------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
+def reverse_words_in(given_string):
+    word_present_index, space_checker, word_ending_index = 0, 0, len(given_string)-1
+    reversed_words = []
+
+    for space in given_string:
+        if space_checker < word_ending_index:
+            
+            if space == ' ':
+                reversed_words.insert(0, given_string[word_present_index:space_checker])
+                space_checker += 1
+                word_present_index = space_checker
+            elif space != ' ':
+                space_checker += 1
+        else:
+            reversed_words.insert(0, given_string[word_present_index:word_ending_index + 1])
+    
+    return ' '.join(reversed_words)
+
+print(reverse_words_in('Alice likes Bob'))
 
 
 
